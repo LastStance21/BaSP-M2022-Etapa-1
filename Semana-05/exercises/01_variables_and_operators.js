55463d786d25980615879e1a6f305862befb22e6
LastStance21 <danycoito@Daniels-MacBook-Air.local>
2022-04-15 17:23:56 -0300

    BaSP-Week5-M2022:first commit

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,37 @@
+/*** 
+*
+1a-Crear dos variables numéricas y utilizar el operador
+suma para guardar el valor de la suma de ambos números en una tercer variable.
+*/
+
+var numadd1 = 25;
+var numadd2 = 50;
+
+var addition = (numadd1 + numadd2);
+console.log('1a ex. result:' + addition);
+
+/***
+ * 
+ 1b- Crear dos variables de tipo String y concatenarlas guardando el
+ resultado en una 3er variable.
+ */
+
+ var strconcat1 = 'Hello';
+ var strconcat2 = 'World';
+
+ var strresult = (strconcat1 + strconcat2)
+ console.log('1b ex. result:' + strresult);
+
+ /***
+  * 
+  1c- Crear dos variables de tipo String y sumar el largo de cada variable 
+(cantidad de letras del string) guardando el resultado de la suma en una 3er
+ variable (utilizar length).
+  */
+
+  var strlenght1 = 'thank';
+  var strlenght2 = 'you';
+
+  var straddlenght = (strlenght1 + strlenght2);
+  console.log('1c ex result:' + straddlenght.length);
+  
