6eeafee5f184d114bf042fd7abfbedbb69cb1307
LastStance21 <danycoito@Daniels-MacBook-Air.local>
2022-05-03 22:03:29 -0300

    BaSP-Week7-M2022:sign up progress with bugs it does not enter the fetch function

--- Semana-07/javascript/signup.js
+++ Semana-07/javascript/signup.js
@@ -42,10 +42,17 @@ function validateDocument(param){
     return true;
   }
 }
-function validateDate(param){
-  var day = new Date();
-  var paramDate = new Date(param);
-  if (paramDate > day){
+// function validateDate(param){
+//   var day = new Date();
+//   var paramDate = new Date(param);
+//   if (paramDate > day){
+//     return false;
+//   }
+// }
+function validateDate(inputD) {
+  var today = new Date();
+  var inputDate = new Date(inputD);
+  if (inputDate > today) {
     return false;
   }
 }
@@ -144,155 +151,242 @@ var rptPasswordError = "";
 window.onload = function () {
   var nameInput = document.getElementById("firstName");
   var errorName = document.getElementById("errornAme");
-  nameInput.addEventListener("blur", function(){
-    if (validateName(nameInput.value) == false){
-      errorName.style.visibility = "visible";
+  function nameEvent() {
+    if (validateName(nameInput.value) == false) {
+      errorName.style.visibility="visible";
       nameError = errorName.textContent;
-    }else{
+    } else {
       nameError = nameInput.value;
     }
+  }
+  nameInput.addEventListener("focus", function() {
+    errorName.style.visibility="hidden";
   })
-  nameInput.addEventListener("focus", function(){
-    errorName.style.visibility = "hidden";
-  })
+  nameInput.addEventListener("blur", nameEvent);
+
   var surnameInput = document.getElementById("lastName");
   var errorSurname = document.getElementById("laName");
-  surnameInput.addEventListener("blur", function(){
-    if (validateName(surnameInput.value) == false){
-      errorSurname.style.visibility = "visible";
+  function surnameEvent() {
+    if (validateName(surnameInput.value) == false) {
+      errorSurname.style.visibility="visible";
       surnameError = errorSurname.textContent;
-    }else{
+    } else {
       surnameError = surnameInput.value;
     }
-  })
-  surnameInput.addEventListener("focus", function(){
-    errorSurname.style.visibility = "hidden";
+  }
+  surnameInput.addEventListener("blur", surnameEvent);
+  surnameInput.addEventListener("focus", function() {
+    errorSurname.style.visibility="hidden";
   })
   var dniInput = document.getElementById("dni");
   var errorDni = document.getElementById("dniError");
-  dniInput.addEventListener("blur", function(){
-    if (validateDocument(dniInput.value) == false){
-      errorDni.style.visibility = "visible";
-      documentError = errorDni.textContent;
-    }else{
+  function documentEvent() {
+    if (validateDocument(dniInput.value) == false) {
+      errorDni.style.visibility="visible";
+      documentError = errorDoc.textContent;
+    } else {
       documentError = dniInput.value;
     }
-  })
-  dniInput.addEventListener("focus", function(){
+  }
+  dniInput.addEventListener("blur", documentEvent);
+  dniInput.addEventListener("focus", function() {
     errorDni.style.visibility="hidden";
   })
   var dateInput = document.getElementById("birthDate");
   var errorDate = document.getElementById("bdError");
-  dateInput.addEventListener("blur", function(){
-    if (validateDate(dateInput.value) == false){
+  function dateEvent() {
+    if (validateDate(dateInput.value) == false) {
       errorDate.style.visibility="visible";
       dateError = errorDate.textContent;
-    }else{
+    } else {
       dateError = dateInput.value;
+      var year = dateError.substring(0,4);
+      var month = dateError.substring(5,7);
+      var day = dateError.substring(8,10);
+      dateError = month + '/' + day + '/' + year;
     }
-  })
-  dateInput.addEventListener("focus", function(){
+  }
+  dateInput.addEventListener("blur", dateEvent);
+  dateInput.addEventListener("focus", function() {
     errorDate.style.visibility="hidden";
   })
   var phoneInput = document.getElementById("phoneNumb");
   var errorPhone = document.getElementById("invalidPhone");
-  phoneInput.addEventListener("blur", function() {
-    if (validatePhone(phoneInput.value) == false){
+  function phoneEvent() {
+    if (validatePhone(phoneInput.value) == false) {
       errorPhone.style.visibility="visible";
       numberError = errorPhone.textContent;
-    }else{
+    } else {
       numberError = phoneInput.value;
     }
-  })
+  }
+  phoneInput.addEventListener("blur", phoneEvent);
   phoneInput.addEventListener("focus", function(){
     errorPhone.style.visibility="hidden";
   })
   var addressInput = document.getElementById("adrress");
   var errorAddress = document.getElementById("addressError");
-  addressInput.addEventListener("blur", function(){
-    if (validateAddress(addressInput.value) == false){
+  function addressEvent() {
+    if (validateAddress(addressInput.value) == false) {
       errorAddress.style.visibility="visible";
       addressError = errorAddress.textContent;
-    }else{
+    } else {
       addressError = addressInput.value;
     }
-  })
+  }
+  addressInput.addEventListener("blur", addressEvent);
   addressInput.addEventListener("focus", function(){
     errorAddress.style.visibility="hidden";
   })
   var cityInput = document.getElementById("location");
   var errorCity = document.getElementById("invalidLocation");
-  cityInput.addEventListener("blur", function(){
-    if (validateCity(cityInput.value) == false){
+  function cityEvent() {
+    if (validateCity(cityInput.value) == false) {
       errorCity.style.visibility="visible";
       cityError = errorCity.textContent;
-    }else{
+    } else {
       cityError = cityInput.value;
     }
-  })
+  }
+  cityInput.addEventListener("blur", cityEvent);
   cityInput.addEventListener("focus", function(){
     errorCity.style.visibility="hidden";
   })
   var postalCodeInput = document.getElementById("psCode");
   var errorPostal = document.getElementById("invalidPsC");
-  postalCodeInput.addEventListener("blur", function(){
-    if (validatePostalCode(postalCodeInput.value) == false){
+  function postalCodeEvent(){
+    if (validatePostalCode(postalCodeInput.value) == false) {
       errorPostal.style.visibility="visible";
       postalError = errorPostal.textContent;
-    }else{
+    } else {
       postalError = postalCodeInput.value;
     }
-  })
+  }
+  postalCodeInput.addEventListener("blur", postalCodeEvent);
   postalCodeInput.addEventListener("focus", function(){
     errorPostal.style.visibility="hidden";
   })
   var emailInput = document.getElementById("Email");
   var errorEmail = document.getElementById("invalidEmail");
-  emailInput.addEventListener("blur", function(){
-    if(emailValidation(emailInput.value) == false){
+  function emailEvent() {
+    if (emailValidation(emailInput.value) == false) {
       errorEmail.style.visibility="visible";
       mailError = errorEmail.textContent;
-    }else{
+    } else {
       mailError = emailInput.value;
     }
-  })
+  }
+  emailInput.addEventListener("blur", emailEvent);
   emailInput.addEventListener("focus", function(){
     errorEmail.style.visibility="hidden";
   })
   var passwordInput = document.getElementById("password");
   var errorPassword = document.getElementById("invalidPassword");
-  passwordInput.addEventListener("blur", function(){
-    if (validatePassword(passwordInput.value) == false){
+  function passwordEvent() {
+    if (validatePassword(passwordInput.value) == false) {
       errorPassword.style.visibility="visible";
       passwordError = errorPassword.textContent;
-    }else{
+    } else {
       passwordError = passwordInput.value;
     }
-  })
+  }
+  passwordInput.addEventListener("blur", passwordEvent);
   passwordInput.addEventListener("focus", function(){{
       errorPassword.style.visibility="hidden";
     }
   })
   var repeatPasswordInput = document.getElementById("pswdconfirm");
   var errorRepeatPassword = document.getElementById("invalidpsswconf");
-  repeatPasswordInput.addEventListener("blur", function(){
-    if (validatePassword(repeatPasswordInput.value) == false){
+  function rptPasswordEvent() {
+    if (validatePassword(repeatPasswordInput.value) == false) {
       errorRepeatPassword.style.visibility="visible";
       rptPasswordError = errorRepeatPassword.textContent;
-    }else{
+    } else {
       rptPasswordError = repeatPasswordInput.value;
     }
-  })
+  }
+  repeatPasswordInput.addEventListener("blur", rptPasswordEvent);
   repeatPasswordInput.addEventListener("focus", function(){
       errorRepeatPassword.style.visibility="hidden";
   })
+    function allValidationsOk() {
+    if (validateName(nameInput.value) && validateDocument(documentInput.value) && 
+      validatePhone(phoneInput.value) && 
+      validateAddress(addressInput.value) && validateCity(cityInput.value) && 
+      validatePostalCode(postalCodeInput.value) && emailValidation(emailInput.value) && 
+      validatePassword(passwordInput.value) && !dateEvent()){
+      return true;
+    } else {
+      alert("Something went wrong, check your info")
+    }
+  }
+    function storageInLocal(){
+    localStorage.setItem("name", nameInput.value);
+    localStorage.setItem("surname", surnameInput.value);
+    localStorage.setItem("document", documentInput.value);
+    localStorage.setItem("dob", dateInput.value);
+    localStorage.setItem("phone", phoneInput.value);
+    localStorage.setItem("address", addressInput.value);
+    localStorage.setItem("city", cityInput.value);
+    localStorage.setItem("zip", postalCodeInput.value);
+    localStorage.setItem("email", emailInput.value);
+    localStorage.setItem("password", passwordInput.value);
+    localStorage.setItem("rptPassword", repeatPasswordInput.value);
+  }
+
+  var signupURL = "https://basp-m2022-api-rest-server.herokuapp.com/signup"
+
+    function confirmFetch() {
+    if (allValidationsOk()) {
+      fetch(signupURL + "?name=" + nameError + "&lastName=" + surnameError + "&dni=" + 
+      documentError + "&dob=" + dateError + "&phone=" + numberError + "&address=" + 
+      addressError + "&city=" + cityError + "&zip=" + postalError + "&email=" + 
+      mailError + "&password=" + passwordError + "&repeatPassword=" + rptPasswordError)
+      .then(function (response) {
+        alert ("Success!")
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+        alert ("Name: " + nameError + "\nSurname: " + surnameError + "\nDocument: " + 
+        documentError + "\nDate of Birth: " + dateError + "\nPhone number: " + 
+        numberError + "\nAddress: " + addressError + "\nLocation: " + cityError + 
+        "\nPostal code: " + postalError + "\nE-mail: " + mailError + "\nPassword: " + 
+        passwordError + "\nRepeat password: " + rptPasswordError);
+      })
+      .catch(function(error) {
+        alert("Sign-up failed", error)
+      })
+    }
+  }
+
+    function showConfirm(event) {
+    event.preventDefault();
+    confirmFetch();
+    storageInLocal();
+  }
+
   var buttonConfirm = document.querySelector('input[type="submit"]');
-  buttonConfirm.addEventListener('click', clickConfirm)
-  function clickConfirm(){
-    alert("Name: " + nameError + "\nSurname: " + surnameError + "\nDocument: " + 
-    documentError + "\nDate of Birth: " + dateError + "\nPhone number: " + 
-    numberError + "\nAddress: " + addressError + "\nLocation: " + cityError + 
-    "\nPostal code: " + postalError + "\nE-mail: " + mailError + "\nPassword: " + 
-    passwordError + "\nRepeat password: " + rptPasswordError)
+  buttonConfirm.addEventListener('click', showConfirm)
+
+  function refreshDataFromLocal(){
+  if (localStorage.name && localStorage.surname && localStorage.document && 
+    localStorage.address && localStorage.dob && localStorage.city && localStorage.zip &&
+    localStorage.phone && localStorage.email && localStorage.password && 
+    localStorage.rptPassword){
+      nameInput.value = localStorage.name;
+      surnameInput.value = localStorage.surname;
+      documentInput.value = localStorage.document;
+      dateInput.value = localStorage.dob;
+      phoneInput.value = localStorage.phone;
+      addressInput.value = localStorage.address;
+      cityInput.value = localStorage.city;
+      postalCodeInput.value = localStorage.zip;
+      emailInput.value = localStorage.email;
+      passwordInput.value = localStorage.password;
+      repeatPasswordInput.value = localStorage.rptPassword;
+    }
   }
-  }
+  refreshDataFromLocal();
+}
+  
